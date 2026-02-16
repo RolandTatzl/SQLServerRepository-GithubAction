80c61d67ef3de7c745de721b7a4c096b553611e4
RolandTatzl <roland.tatzl@solicon-it.com>
2026-02-16 18:31:59 +0100

    Initial Version

---
+++ GithubAction/DWH_Staging/meta/Tables/TablesINFOR.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [meta].[TablesINFOR] (
+    [tabinID]                     SMALLINT      IDENTITY (1, 1) NOT NULL,
+    [tabin_SchemaSOURCE]          NVARCHAR (50) NULL,
+    [tabin_TableNameSOURCE]       NVARCHAR (50) NOT NULL,
+    [tabin_Active]                BIT           NOT NULL,
+    [tabin_TableLoadPRIORITY]     SMALLINT      NOT NULL,
+    [tabin_serv_ClientID]         SMALLINT      NOT NULL,
+    [tabin_BusinessCompanyNumber] NVARCHAR (3)  NOT NULL,
+    [tabin_bcnr_BCNRange]         INT           NOT NULL,
+    [tabin_TableNameTARGET]       NVARCHAR (50) NOT NULL,
+    [tabin_TableCommonName]       NVARCHAR (50) NULL,
+    [tabin_IncrementDays]         SMALLINT      CONSTRAINT [DF_TablesINFOR_tabin_IncrementDays] DEFAULT ((0)) NOT NULL,
+    [tabin_IncrementColumn]       NVARCHAR (10) NULL,
+    CONSTRAINT [PK_TablesINFOR] PRIMARY KEY CLUSTERED ([tabin_TableNameSOURCE] ASC) WITH (FILLFACTOR = 90)
+);
+
